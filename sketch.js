2b869a3f2b39dc1c5ab7bd5680625173de516323
VaibhavMishra333 <75656581+VaibhavMishra333@users.noreply.github.com>
2021-04-19 16:11:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,57 @@
-var fixedRect, movingRect;
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var stone, iron,rubber,hammer;
+var sand1, sand2, sand3, sand4,sand5,sand6;
 
 function setup(){
-createCanvas(1200,800);
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
 
-fixedRect = createSprite(200,200,50,80);
-fixedRect.shapeColor="green";
-fixedRect.debug = true;
+    plane = new Plane(600,height,1200,20)
+    
+    stone = new Stone(700,320,100,100);
+    iron = new Iron(300,350);
+    rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
 
-movingRect = createSprite(400,200,80,30);
-movingRect.shapeColor = "green";
-movingRect.debug = true;
-}
-function draw (){
-background(0,0,0);
+    sand1 = new Sand(505,450,10);
+    sand2 = new Sand(510,450,10);
+    sand3 = new Sand(515,450,10);
+    sand4 = new Sand(520,450,10);
+    sand5 = new Sand(525,450,10);
+    sand6 = new Sand(515,445,10);
+    sand7 = new Sand(520,445,10);
+    sand8 = new Sand(525,445,10);
+    sand9 = new Sand(530,445,10);
+    sand10 = new Sand(535,445,10);
 
-movingRect.x = World.mouseX;
-movingRect.y=  World.mouseY;
 
-if(movingRect.x - fixedRect.x <fixedRect.width/2 + movingRect.width/2
-    && fixedRect.x - movingRect.x <fixedRect.width/2 + movingRect.width/2
-    && movingRect.y - fixedRect.y <fixedRect.width/2 + movingRect.width/2
-    && fixedRect.y - movingRect.y <fixedRect.width/2 + movingRect.width/2){
-        movingRect.shapeColor = "red";
-        fixedRect.shapeColor = "red"
-    }
-
-else{
-movingRect.shapeColor= "green";
-fixedRect.shapeColor = "green";
 }
 
-drawSprites();
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    stone.display();
+    plane.display();
+    iron.display();
+    rubber.display();
+
+    hammer.display();
+
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
 }
